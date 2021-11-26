43b2081c9481b320292c4d2aa0e0054cd7f4f5a7
andrevale99 <andrefvale@gmail.com>
2021-11-26 15:33:57 -0300

    ...

--- ELE2715/ULA/TESTBENCH.vhd
+++ ELE2715/ULA/TESTBENCH.vhd
@@ -15,43 +15,47 @@ architecture bench of tb is
 				Y : out bit_vector(7 downto 0));
 	end component;
 
+	component ADD8 is
+		port (A, B : in bit_vector(7 downto 0);
+				Ci : in bit;
+				Co : out bit;
+				S : out bit_vector(7 downto 0));
+	end component;
+
 	signal A : bit_vector(7 downto 0);
 	signal SEL : bit_vector(2 downto 0);
 	signal F : bit_vector(7 downto 0);
 	signal F2 : bit_vector(7 downto 0);
 
-	for lab : SHFTL use entity work.SHFTL;
-	for lab_2 : SHFTR use entity work.SHFTR;
+	signal AULA : bit_vector(7 downto 0);
+	signal BULA : bit_vector(7 downto 0);
+	signal CIULA : bit;
+
+	for DESL_LEFT : SHFTL use entity work.SHFTL;
+	for DESL_RIGHT : SHFTR use entity work.SHFTR;
+	for SOMA : ADD8 use entity work.ADD8;
 
 	begin
 
-		lab : SHFTL port map (I => A, S => SEL, Y => F);
-		lab_2 : SHFTR port map (I => A, S=> SEL, Y => F2);
+		DESL_LEFT : SHFTL port map (I => A, S => SEL, Y => F);
+		DESL_RIGHT : SHFTR port map (I => A, S=> SEL, Y => F2);
+		SOMA : ADD8 port map (A => AULA, B => BULA, Ci => CIULA);
 
 	process
 	begin
 
-		A <= "11111111";
-		SEL <= "000";
-		wait for 100 fs;
-	
-		A <= "11111111";
-		SEL <= "010";
+		AULA <= "00000001";
+		BULA <= "00000001";
+		CIULA <= '0';
 		wait for 100 fs;
 
-		A <= "11111111";
-		SEL <= "100";
-		wait for 100 fs;
-
-		A <= "11111111";
-		SEL <= "101";
-		wait for 100 fs;
-
-		A <= "11111111";
-		SEL <= "110";
+		AULA <= "00001000";
+		BULA <= "01000000";
+		CIULA <= '0';
 		wait for 100 fs;
 
 		wait;
 		end process;
 
 end bench;
+
