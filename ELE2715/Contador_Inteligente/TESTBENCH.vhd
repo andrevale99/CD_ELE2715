a5db7bb8409c52cd0ecce4b742c38f46983a9da4
andrevale99 <andrefvale@gmail.com>
2021-12-08 16:27:10 -0300

    ...

--- ELE2715/Contador_Inteligente/TESTBENCH.vhd
+++ ELE2715/Contador_Inteligente/TESTBENCH.vhd
@@ -9,39 +9,56 @@ architecture bench of tb is
 				S : in bit;
 				Y : out bit);
 	end component;
+
+	component ADD4 is
+		port(A, B : in bit_vector(3 downto 0);
+	   			Co : out bit;
+				S : out bit_vector(3 downto 0));
+	end component;
+
+	component SUB4 is
+		port (A, B : in bit_vector(3 downto 0);
+			Tout : out bit;
+			S : out bit_vector(3 downto 0));
+	end component;
 	
 
 	--SINAIS
 	signal A : bit_vector(3 downto 0);
+	signal B : bit_vector(3 downto 0);
 	signal SEL : bit;
 	
 
 	--for DESL_LEFT : SHFTL use entity work.SHFTL;
-	for FINAL : TC use entity work.TC;
+	for ESTOURO : TC use entity work.TC;
+	for SOMA : ADD4 use entity work.ADD4;
+	for SUB : SUB4 use entity work.SUB4;
 
 	begin
 
 	--DESL_LEFT : SHFTL port map (I => A, S => SEL, Y => F);
-	FINAL : TC port map (ENTRADA => A, S => SEL);
+	ESTOURO : TC port map (ENTRADA => A, S => SEL);
+	SOMA : ADD4 port map (A => A, B=>B);
+	SUB : SUB4 port map (A=>A, B=> B);
 
 	process
 	begin
 
-		A <= "1111";
-		SEL <= '0';
-		wait for 10 ns;
+		A <= "1110";
+		B <= "0100";
+		wait for 10 fs;
+
+		A <= "1001";
+		B <= "0011";
+		wait for 10 fs;
 
 		A <= "1111";
-		SEL <= '1';
-		wait for 10 ns;
+		B <= "1111";
+		wait for 10 fs;
 
-		A <= "0000";
-		SEL <= '0';
-		wait for 10 ns;
-
-		A <= "0000";
-		SEL <= '1';
-		wait for 10 ns;
+		A <= "1000";
+		B <= "0111";
+		wait for 10 fs;
 
 		wait;
 		end process;
