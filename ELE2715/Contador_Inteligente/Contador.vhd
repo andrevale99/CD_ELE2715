ddee317cb47cb9acf8175fb9362227971461221a
andrevale99 <andrefvale@gmail.com>
2021-12-09 10:12:16 -0300

    Coloquei os FF's D e J-K num único arquivo

--- ELE2715/Contador_Inteligente/Contador.vhd
+++ ELE2715/Contador_Inteligente/Contador.vhd
@@ -259,3 +259,63 @@ architecture ckt of SUB4 is
 		R3 : SUBCOMP port map (A(3), B(3), Borrow(2), S(3), Borrow(3));
 
 end ckt;
+
+--==============================================================================================
+--				FLIP-FLOP D
+--==============================================================================================
+entity FFD is
+	port (clk ,d,set ,clr: in bit;
+			q: out bit);
+end FFD;
+
+architecture CKT of FFD is
+	
+signal qs: bit;
+begin
+	process(ck ,set ,clr
+	begin
+
+		if set = ’0’ then qs <= ’1’;
+		elsif clr = ’0’ then qs <= ’0’;
+		elsif ck=’1’ and ck ’event then
+			qs <= d;
+	end if;
+
+	end process;
+
+	q <= qs;
+
+end CKT;
+
+
+--==============================================================================================
+--				FLIP-FLOP J-K
+--==============================================================================================
+entity FFJK is
+	port (clk ,j,k,set ,clr: in bit;
+		q: out bit);
+end FFJK;
+
+
+architecture CKT of FFJK is
+
+signal qs: bit;
+begin
+
+	process(ck ,set ,clr)
+	begin
+		if set = ’0’ then qs <= ’1’;
+		elsif clr = ’0’ then qs <= ’0’;
+		elsif ck=’1’ and ck ’event then
+			
+			if j = ’1’ and k = ’1’ then qs <= not qs;
+			elsif j = ’1’ and k = ’0’ then qs <= ’1’;
+			elsif j = ’0’ and k = ’1’ then qs <= ’0’;
+			end if;
+
+		end if;
+	end process;
+
+	q <= qs;
+
+end CKT;
