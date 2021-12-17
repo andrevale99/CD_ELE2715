699ea4d1b47a0f70da0fd8ccbc035e123f86c5df
andrevale99 <andrefvale@gmail.com>
2021-12-16 21:53:08 -0300

    ...

--- ELE2715/ULA/TESTBENCH.vhd
+++ ELE2715/ULA/TESTBENCH.vhd
@@ -5,61 +5,28 @@ end tb;
 
 architecture bench of tb is
 
-	component ADD8 is
-		port (A, B : in bit_vector(7 downto 0);
-				Ci : in bit;
+	component MULT is
+		port(A, B : in bit_vector(7 downto 0);
 				Co : out bit;
-				S : out bit_vector(7 downto 0);
-				Z : out bit);
-	end component;
-
-	component BARREL_RIGHT is
-		port (S : in bit_vector(2 downto 0);
-			I : in bit_vector(7 downto 0);
-			Y : out bit_vector(7 downto 0));
-	end component;
-
-	component BARREL_LEFT is
-		port (S : in bit_vector(2 downto 0);
-			I : in bit_vector(7 downto 0);
-			Y : out bit_vector(7 downto 0));
-	end component;
-
-	component SUB8 is
-		port (A, B : in bit_vector(7 downto 0);
-				Ti : in bit;
-				Tout : out bit;
 				S : out bit_vector(7 downto 0));
 	end component;
 
-	signal A : bit_vector(7 downto 0);
-	signal B : bit_vector(7 downto 0);
-	signal B_3 : bit_vector(2 downto 0);
-	signal Cin : bit;
+	signal a : bit_vector(7 downto 0);
+	signal b : bit_vector(7 downto 0);
 
-	for SOMA : ADD8 use entity work.ADD8;
-	for SUB : SUB8 use entity work.SUB8;
-	for DESC_L : BARREL_LEFT use entity work.BARREL_LEFT;
-	for DESC_R : BARREL_RIGHT use entity work.BARREL_RIGHT;
-
+	for MUMU : MULT use entity work.MULT;
 
 	begin
 
-	SOMA : ADD8 port map (A=>A, B=>B, Ci => Cin);
-	SUB : SUB8 port map (A=>A, B=>B, Ti => '0');
-	DESC_L : BARREL_LEFT port map (S=>B_3, I=>A);
-	DESC_R : BARREL_RIGHT port map (S=>B_3, I=>A);
+	MUMU : MULT port map (A=>a, B=>b);
 	
 	process
 	begin
 
-		A <= "11111111";
-		B <= "10000001";
+		a <= "00001001";
+		b <= "00001001";
 		wait for 10 ns;
 
-		A <= "00000111";
-		B <= "00000011";
-		wait for 10 ns;
 
 		wait;
 		end process;
