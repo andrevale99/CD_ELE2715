9a17c3e7d82ecaf1bb797a4914551e5a670171be
andrevale99 <andrefvale@gmail.com>
2022-01-15 23:06:14 -0300

    Finalizar lógica para parar a contagem

--- ELE2715/Cofre_DIgital/Componentes/Contador20s.vhd
+++ ELE2715/Cofre_DIgital/Componentes/Contador20s.vhd
@@ -9,8 +9,8 @@ end CONT20;
 architecture ckt of CONT20 is
 
     component FFJK is
-        port (clk ,j,k,set ,clr: in bit;
-                q: out bit);
+        port (clk ,j,k,set ,clr, en: in bit;
+		q: out bit);
     end component;
 
     signal saida : bit_vector(4 downto 0);
@@ -19,17 +19,17 @@ architecture ckt of CONT20 is
 
     begin
 
-        C0 : FFJK port map (CLK, '1', '1', '1', CLR, saida(0));
-        C1 : FFJK port map (CLK, saida(0), saida(0), CLR, saida(1));
+        C0 : FFJK port map (CLK, '1', '1', '1', '1', CLR, saida(0));
+        C1 : FFJK port map (CLK, saida(0), saida(0), '1', '1', CLR, saida(1));
 
         vdd(0) <= saida(0) and saida(1);
-        C2 : FFJK port map (CLK, vdd(0), vdd(0), CLR, saida(2));
+        C2 : FFJK port map (CLK, vdd(0), vdd(0), '1', '1', CLR, saida(2));
 
         vdd(1) <= saida(2) and vdd(0);
-        C3 : FFJK port map (CLK, vdd(1), vdd(1), CLR, saida(3));
+        C3 : FFJK port map (CLK, vdd(1), vdd(1), '1', '1', CLR, saida(3));
 
         vdd(2) <= saida(3) and vdd(1);
-        C4 : FFJK port map (CLK, vdd(2), vdd(2), CLR, saida(4));
+        C4 : FFJK port map (CLK, vdd(2), vdd(2), '1', '1', CLR, saida(4));
 
         C(0) <= saida(0);
         C(1) <= saida(1);
--- ELE2715/Cofre_DIgital/Componentes/Contador5s.vhd
+++ ELE2715/Cofre_DIgital/Componentes/Contador5s.vhd
@@ -9,8 +9,8 @@ end CONT5;
 architecture ckt of CONT5 is
 
     component FFJK is
-        port (clk ,j,k,set ,clr: in bit;
-                q: out bit);
+        port (clk ,j,k,set ,clr, en: in bit;
+		        q: out bit);
     end component;
 
     signal saida : bit_vector(2 downto 0);
@@ -19,9 +19,9 @@ architecture ckt of CONT5 is
 
     begin
 
-        C0 : FFJK port map (CLK, '1', '1', '1', CLR, saida(0));
-        C1 : FFJK port map (CLK, saida(0), saida(0), CLR, saida(1));
-        C2 : FFJK port map (CLK, vdd, vdd, CLR, saida(2));
+        C0 : FFJK port map (CLK, '1', '1', '1', '1', CLR, saida(0));
+        C1 : FFJK port map (CLK, saida(0), saida(0), '1', '1', CLR, saida(1));
+        C2 : FFJK port map (CLK, vdd, vdd, '1', '1', CLR, saida(2));
 
         vdd <= saida(0) and saida(1);
 
