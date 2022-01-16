6fce87c515e5f9209c9d2d4c8236d47e31282adb
andrevale99 <andrefvale@gmail.com>
2022-01-15 23:06:31 -0300

    Acrescentado o enable

--- ELE2715/Cofre_DIgital/Componentes/FlipFlop_JK.vhd
+++ ELE2715/Cofre_DIgital/Componentes/FlipFlop_JK.vhd
@@ -1,5 +1,5 @@
 entity FFJK is
-	port (clk ,j,k,set ,clr: in bit;
+	port (clk ,j,k,set ,clr, en: in bit;
 		q: out bit);
 end FFJK;
 
@@ -9,11 +9,11 @@ architecture CKT of FFJK is
 signal qs: bit;
 begin
 
-	process(clk ,set ,clr)
+	process(clk ,set ,clr, en)
 	begin
 		if set = '0' then qs <= '1';
 		elsif clr = '0' then qs <= '0';
-		elsif clk= '1' and clk 'event then
+		elsif clk= '1' and clk 'event and en = '1' then
 			
 			if j = '1' and k = '1' then qs <= not qs;
 			elsif j = '1' and k = '0' then qs <= '1';
