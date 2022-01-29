a5b99d7ba77e3645fc5a9f3991851bc438d3abbd
andrevale99 <andrefvale@gmail.com>
2022-01-29 16:55:41 -0300

    Criação dos FF's

---
+++ ELE2715/Maquina_de_troco/Componentes/FlipFlop_D.vhd
@@ -0,0 +1,23 @@
+entity FFD is
+	port (clk ,d,set ,clr, en: in bit;
+			q: out bit);
+end FFD;
+
+architecture CKT of FFD is
+	
+signal qs: bit;
+begin
+	process(clk ,set ,clr)
+	begin
+
+		if set = '0' then qs <= '1';
+		elsif clr = '0' then qs <= '0';
+		elsif clk='1' and clk 'event and en = '1' then
+			qs <= d;
+	end if;
+
+	end process;
+
+	q <= qs;
+
+end CKT;
