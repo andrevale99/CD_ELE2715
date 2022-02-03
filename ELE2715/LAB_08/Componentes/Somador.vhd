6b2dde0d98d95465fbb53532d9c2ffcbf82ca284
andrevale99 <andrefvale@gmail.com>
2022-02-03 17:53:25 -0300

    Somador de 8 e 10 bits

--- ELE2715/LAB_08/Componentes/Somador.vhd
+++ ELE2715/LAB_08/Componentes/Somador.vhd
@@ -53,7 +53,7 @@ architecture ckt of ADD8 is
 				S, Co : out bit);
 	end component;
 
-	signal Carry : bit_vector(8 downto 0);
+	signal Carry : bit_vector(6 downto 0);
 
 	begin
 
@@ -68,4 +68,40 @@ architecture ckt of ADD8 is
 
 end ckt;
 
+--==============================================================================================
+--				SOMADOR DE 10 BITS (usado no final do filtro)
+--==============================================================================================
 
+entity ADD10 is
+	port (	A, B : in bit_vector(9 downto 0);
+			Co : out bit;
+			S : out bit_vector(9 downto 0));
+end ADD10;
+
+architecture ckt of ADD10 is
+	component SCOMP is
+		port ( A, B, Ci : in bit;
+				S, Co : out bit);
+	end component;
+
+	component SMEIO is
+		port (A, B : in bit;
+				S, Co : out bit);
+	end component;
+
+	signal Carry : bit_vector(8 downto 0);
+
+begin
+
+	R0 : SMEIO port map (A(0), B(0), S(0), Carry(0));		
+	R1 : SCOMP port map (A(1), B(1), Carry(0), S(1), Carry(1));
+	R2 : SCOMP port map (A(2), B(2), Carry(1), S(2), Carry(2));
+	R3 : SCOMP port map (A(3), B(3), Carry(2), S(3), Carry(3));
+	R4 : SCOMP port map (A(4), B(4), Carry(3), S(4), Carry(4));
+	R5 : SCOMP port map (A(5), B(5), Carry(4), S(5), Carry(5));
+	R6 : SCOMP port map (A(6), B(6), Carry(5), S(6), Carry(6));
+	R7 : SCOMP port map (A(7), B(7), Carry(6), S(7), Carry(7));
+	R8 : SCOMP port map (A(8), B(8), Carry(7), S(8), Carry(8));
+	R9 : SCOMP port map (A(9), B(9), Carry(8), S(9), Co);
+
+end ckt;
