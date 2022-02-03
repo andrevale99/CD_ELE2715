50faab07541dc37152c9f20d515aaa97ead9e0f2
andrevale99 <andrefvale@gmail.com>
2022-02-03 17:53:17 -0300

    Registrador Para os Y's

--- ELE2715/LAB_08/Componentes/RegY.vhd
+++ ELE2715/LAB_08/Componentes/RegY.vhd
@@ -3,7 +3,7 @@
 --====================================================
 entity REGY is
 	port (	Y : in bit_vector(3 downto 0);
-			clk, clr, ld : in bit;
+			clk, clr_r, ld_r : in bit;
 			Yout : out bit_vector(3 downto 0));
 end REGY;
 
@@ -17,36 +17,6 @@ architecture ckt of REGY is
 
 	begin 
 
-		SAIDA : MEM4B port map (Y, clk, clr, ld, Yout);
+		SAIDA : MEM4B port map (Y, clk, clr_r, ld_r, Yout);
 
 end ckt;
-
---====================================================
---	REGISTRADOR PARA ARMAZENAR OS 3 VALORES DE Y
---====================================================
-
-entity REGY3 is
-	port ( Y : in bit_vector(3 downto 0);
-			clk, clr, ld : bit;
-			Yout : out bit_vector(3 downto 0));
-end REGY3;
-
-architecture ckt of REGY3 is
-
-	component REGY is
-		port (	Y : in bit_vector(3 downto 0);
-				clk, clr, ld : in bit;
-				Yout : out bit_vector(3 downto 0));
-	end component;
-
-	signal t0 : bit_vector(3 downto 0);
-	signal t1 : bit_vector(3 downto 0);
-
-	begin
-
-		YT0 : REGY port map (Y, clk, clr, ld, t0);
-		YT1 : REGY port map (t0, clk, clr, ld, t1);
-		YT2 : REGY port map (t1, clk, clr, ld, Yout);
-
-end ckt ; 
-
