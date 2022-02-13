4d5f937020def8772f931cc852770c5ebd03a483
andrevale99 <andrefvale@gmail.com>
2022-02-13 10:43:32 -0300

    ...

--- ELE2715/Memoria/TESTBENCH.vhd
+++ ELE2715/Memoria/TESTBENCH.vhd
@@ -1,80 +1,95 @@
 --COMANDO no terminal para compilar e visuzalizar no GTKWAVE
---ghdl -a Componentes/*.vhd  && ghdl -a TESTBENCH.vhd && ghdl -e tb && ghdl -r tb --vcd=wave.simu--stop-time=10sec && gtkwave wave.simu
+--ghdl -a Componentes/*.vhd  && ghdl -a TESTBENCH.vhd && ghdl -e tb && ghdl -r tb --vcd=wave.simu --stop-time=10sec && gtkwave wave.simu
 
 entity tb is
 end tb;
 
 architecture bench of tb is
 
-	component REG16X13 is
-		port (  clk, clr : in bit;
-				wd , rd : in bit;
-				wd_addr, rd_addr : in bit_vector(3 downto 0);
-				wd_data : in bit_vector(12 downto 0);
-				rd_data : out bit_vector(12 downto 0));
+	component FIFO is
+		port ( clk, wr, rd, clr_fifo : in bit;
+				w_data : in bit_vector(12 downto 0);
+				empty, full : out bit;
+				r_data : out bit_vector(12 downto 0));
 	end component;
 
-	signal clk: bit := '0'; --CLOCK
+	signal wr, rd, clr_fifo : bit;
+	signal wdata : bit_vector(12 downto 0);
 
-	signal clr : bit;
-	signal wd, rd : bit;
-	signal wd_addr, rd_addr : bit_vector(3 downto 0);
-	signal wd_data : bit_vector(12 downto 0);
+	signal clk: bit := '0'; --CLOCK
 
 
 	--EXEMPLO de como criar os componentes para visualização no GTKWAVE
 	--for MUX8 : MUX8B use entity work.MUX8B;
-	for R16x13 : REG16X13 use entity work.REG16X13;
+	for FF : FIFO use entity work.FIFO;
 
 
 	begin
 	--EXEMPLO de como criar os componentes para visualização no GTKWAVE
 	--MUX8 : MUX8B port map (i=>a, s=>S);
 	CLOCK : clk <=  not clk after 0.250 sec;
-	R16x13 : REG16X13 port map (clk=>clk, clr=>clr, wd=>wd, rd=>rd, wd_addr=>wd_addr, rd_addr=>rd_addr, wd_data=>wd_data);
+	FF : FIFO port map (clk=>clk, wr=>wr, rd=>rd, clr_fifo=>clr_fifo, w_data=>wdata);
 
 	process
 	begin
 
-		clr <= '1';
-		wd_data <= "1111111111111";
+		
 
-		wd <= '1';
-		wd_addr <= "0000";
-		wait for 1 sec;
+		clr_fifo <= '1';
+		wr <= '0';
+		wait for  500 ms;
 
-		wd_data <= "0101010101010";
-		wd <= '1';
-		wd_addr <= "0101";
-		wait for 1 sec;
+		wr <= '1';
+		wdata <= "0000000000111";
+		wait for 500 ms;
 
-		wd_data <= "0000000000011";
-		wd <= '1';
-		wd_addr <= "1111";
-		wait for 1 sec;
+		wdata <= "0000000100111";
+		wait for 500 ms;
 
-		wd <= '0';
-		wait for 1 sec;
+		wdata <= "0000100100111";
+		wait for 500 ms;
 
+		wdata <= "0100000100111";
+		wait for 500 ms;
+
+		wdata <= "0000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1010110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000111100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+		
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wdata <= "1000110100111";
+		wait for 500 ms;
+
+		wr <= '0';
 		rd <= '1';
-		rd_addr <= "0000";
-		wait for 1 sec;
-
-		rd_addr <= "0001";
-		wait for 1 sec;
-
-		rd_addr <= "0101";
-		wait for 1 sec;
-
-		rd_addr <= "1111";
-		wait for 1 sec;
-
-		rd <= '0';
-		rd_addr <= "0000";
-		wait for 4 sec;
-
-		clr <= '0';
-		wait for 1 sec;
 
 
 		wait;
