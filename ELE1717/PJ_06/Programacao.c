061770390f5ea6fac8aaebcd2dd68cf4a3ae4782
andrevale <andrefvale@gmail.com>
2022-07-21 22:04:55 -0300

    .

---
+++ ELE1717/PJ_06/Programacao.c
@@ -0,0 +1,322 @@
+#define F_CPU 16000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ *	@brief Altera a senha da central
+ * */
+void alterar_senha()
+{
+	uint8_t leitura = 0x00; //Variavel para ler as portas
+   
+    leitura = PINC;
+
+    PORTB ^= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+    //Laço de colocar a senha ATUAL
+
+	//Verifica se pressinou "A"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou as teclas de "2"
+	do
+	{
+		leitura = PINC;
+	}while((leitura & 0x01) == 0x01);
+
+
+	//Verifica se pressionou as teclas de "0" ate "3"
+	do
+	{
+		leitura = PINC;
+	}while((leitura & 0x02) == 0x02);
+
+	//Laço para colocar a NOVA SENHA
+	
+	//Verifica se pressionou as teclas de "E"
+	do
+	{
+		leitura = PINC;
+	}while((leitura & 0x01) == 0x01);
+
+    PORTB ^= (1<<PB5);
+    _delay_ms(1000);
+}
+
+/**
+ *	@brief Habilita/Desabilita sensor
+ * */
+void habilitar_sensor()
+{
+	uint8_t leitura = 0x00;
+
+	leitura = PINC;
+	
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laço para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+		//	Falta colocar a logica para desabilitar sensor
+		//o lugar de pressionar "A" o usuario pressiona "D"
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "2"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Verifica se pressionou a tecla de "0" ate "7"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	PORTB &= ~(1<<PB5);
+	_delay_ms(1000);
+
+}
+
+
+/**
+ *	@brief Associa um sensor a uma zona
+ * */
+void associar_sensor_zona()
+{
+	uint8_t leitura = 0x00;
+	leitura = PINC;
+
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laco para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "4"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Verifica se pressionou a tecla de "0" ate "7"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla de "1" ate "3"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	PORTB ^= (1<<PB5);
+	_delay_ms(1000);
+}
+
+/**
+ *	@brief Habilita uma zona
+ * */
+void habilitar_zona()
+{
+    uint8_t leitura = 0x00;
+	leitura = PINC;
+
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laco para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+        //	Falta colocar a logica para desabilitar sensor
+		//o lugar de pressionar "A" o usuario pressiona "D"
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "5"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Verifica se pressionou a tecla de "1" ate "3"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	PORTB ^= (1<<PB5);
+	_delay_ms(1000);
+}
+
+/**
+ *	@brief Ajusta o temporizador
+ * */
+void ajustar_temporizador()
+{
+    uint8_t leitura = 0x00;
+	leitura = PINC;
+
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laco para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "6"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+    //Laco para colocar 3 valores de "0" a "9"
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	PORTB ^= (1<<PB5);
+	_delay_ms(1000);
+}
+
+/**
+ *	@brief Ajusta o temporizador do timeout
+ * */
+void ajustar_timeout()
+{
+    uint8_t leitura = 0x00;
+	leitura = PINC;
+
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laco para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "7"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+    //Laco para colocar 2 valores de "0" a "9"
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	PORTB ^= (1<<PB5);
+	_delay_ms(1000);
+}
+
+void ajustar_tempo_sirene()
+{
+    uint8_t leitura = 0x00;
+	leitura = PINC;
+
+	PORTB |= (1<<PB5);
+	//Verifica se pressionou a tecla "P"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+	//Laco para colocar a senha ATUAL
+
+	//Verifica se pressionou a tecla "A"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	//Verifica se pressionou a tecla "8"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x01) == 0x01);
+
+    //Laco para colocar 3 valores de "0" a "9"
+
+	//Verifica se pressionou a tecla "E"
+    do
+    {
+    	leitura = PINC;
+    }while((leitura & 0x02) == 0x02);
+
+	PORTB ^= (1<<PB5);
+	_delay_ms(1000);
+}
